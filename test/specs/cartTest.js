72fec0029ff79f7d9efe0dd0dcf7e448b7e2816f
jrmuffun <jrmuffun12@gmail.com>
2025-10-31 16:08:48 -0600

    Initial Commit

---
+++ test/specs/cartTest.js
@@ -0,0 +1,18 @@
+import Login from '../pageobjects/loginPage.js';
+import ProductPage from '../pageobjects/productPage.js';
+import CartPage from '../pageobjects/cartPage.js';
+import Webpage from '../pageobjects/url.js'
+import {browser} from '@wdio/globals'
+
+describe('Hamburger Menu Buttons Test', () => {
+    it('should have all buttons function as intended', async () => {
+        // NO EXPECTS IN THIS FILE, MUST BE IN METHODS
+        // Login as a setup
+        // Add items to cart (probably all of them)
+        // Assert cart icon number of items
+        // Click cart icon
+        // Remove items from cart (Probably like 4)
+        // Assert cart icon quantity
+        // 
+    })
+})
